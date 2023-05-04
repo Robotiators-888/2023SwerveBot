3a5648193350849d5e69e4f6d7d099e8b0f7eb8d
Spencer McGrath <Samcgrath2005@gmail.com>
2023-05-04 15:26:24 -0400

    Actually fixed the file path

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.Constants.OIConstants;
+import frc.robot.Subsystems.SUB_Drivetrain;
+import frc.robot.Subsystems.SUB_SwerveModuleTest;
+
+public class RobotContainer {
+
+  //SUB_Drivetrain drivetrain = new SUB_Drivetrain();
+  SUB_SwerveModuleTest test = new SUB_SwerveModuleTest();
+  Joystick joystick = new Joystick(0);
+
+  public RobotContainer() {
+    configureBindings();
+
+    // Configure default commands
+    // drivetrain.setDefaultCommand(
+    //     new RunCommand(
+    //         () -> drivetrain.drive(
+    //             -MathUtil.applyDeadband(joystick.getRawAxis(1), OIConstants.kDriveDeadband),
+    //             -MathUtil.applyDeadband(joystick.getRawAxis(0), OIConstants.kDriveDeadband),
+    //             -MathUtil.applyDeadband(joystick.getRawAxis(2), OIConstants.kDriveDeadband),
+    //             true, true),
+    //             drivetrain));
+
+    test.setDefaultCommand(
+        new RunCommand(
+            () -> test.drive(
+                -MathUtil.applyDeadband(joystick.getRawAxis(1), OIConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(joystick.getRawAxis(0), OIConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(joystick.getRawAxis(2), OIConstants.kDriveDeadband),
+                true, true),
+                test));
+
+
+
+  }
+
+  private void configureBindings() {}
+
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
+}
