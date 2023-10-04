d67e384fdffae9377f791139ee9084bc7f2ba6e9
KevinH45 <timmyhwang127@gmail.com>
2023-10-03 23:29:19 -0400

    commit before rebase

--- src/main/java/frc/robot/Subsystems/SUB_Limelight.java
+++ src/main/java/frc/robot/Subsystems/SUB_Limelight.java
@@ -7,10 +7,13 @@ package frc.robot.Subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.geometry.Pose3d;
 
 public class SUB_Limelight extends SubsystemBase {
   private NetworkTable table;
@@ -22,17 +25,20 @@ public class SUB_Limelight extends SubsystemBase {
 
   /**
    * Returns robot pose in terms of x,y,z position on the field in meters, and roll, pitch yaw, in degrees. 
-   * @return Botpose network tables entry (X, Y, Z (meters), ROLL, PITCH, YAW (degrees))
+   * @implNote (X, Y, Z (meters), ROLL, PITCH, YAW (degrees))
+   * @return Botpose network tables entry in Pose3d
    */
-  public Pose2d getPose(){
+  public Pose3d getPose(){
     NetworkTableEntry entry = table.getEntry("botpose");
     double[] poseArr = entry.getDoubleArray(new double[0]);
 
-    if (poseArr.length > 0){
+    // If the pose array is empty/contains the default array (double[0])
+    if (poseArr.length == 0){
       return null;
     }
+
     // x,y and -yaw.
-    return new Pose2d(poseArr[0], poseArr[1], new Rotation2d(-poseArr[5]));
+    return new Pose3d(new Pose2d(poseArr[0], poseArr[1], new Rotation2d(-poseArr[5])));
   }
 
   @Override
@@ -66,16 +72,28 @@ public class SUB_Limelight extends SubsystemBase {
     return table.getEntry("ty").getDouble(0);
   }
 
-  // find h1, h2, a1 of current limelight position
-  
-  // public double getDistance() {
-  //   double h1 = 35.125;
-  //   double h2 = 111;
-  //   double a1 = 0.4625123;
-  //   double a2 = Math.toRadians(this.getTy());
+  /**
+   * Returns target pose in terms of x,y,z position on the field in meters, and roll, pitch yaw, in degrees. 
+   * @implNote We assume that the array is shaped (X, Y, Z (meters), ROLL, PITCH, YAW (degrees)), however, ENSURE that this is the case.
+   * @return Get the 3D transform needed to get to the target in Pose3D
+   */
+  public Transform3d getTargetTransform(){
+    NetworkTableEntry entry = table.getEntry("target-pose_cameraspace");
+    double[] resultArr = entry.getDoubleArray(new double[0]);
 
-  //   return (double) ((h2 - h1) / (Math.tan(a1 + a2)));
-  // }
+    // If the result array is empty/contains the default array (double[0])
+    if (resultArr.length == 0){
+      return null;
+    }
+
+    Translation3d transl3d = new Translation3d(resultArr[0], resultArr[1], resultArr[2]);
+    Rotation3d rot3d = new Rotation3d(resultArr[3], resultArr[4], resultArr[5]);
+    // x,y and -yaw.
+
+    return 
+  }
+ 
+
   /**
    * Sets LED Mode
    */
