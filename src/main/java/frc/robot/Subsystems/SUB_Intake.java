4e4cd915c7e80df8a4e49f40cd7638bba00fbb65
KevinH45 <timmyhwang127@gmail.com>
2023-10-20 19:14:24 -0400

    fmc 31c937a

--- src/main/java/frc/robot/Subsystems/SUB_Intake.java
+++ src/main/java/frc/robot/Subsystems/SUB_Intake.java
@@ -15,13 +15,13 @@ public class SUB_Intake extends SubsystemBase {
   private final static TalonSRX intakeMotor = new TalonSRX(Constants.Manuiplator.kMANUIP_INTAKE_MOTOR_CANID);
 
   public static void intakeIn(){
-    intakeMotor.set(TalonSRXControlMode.PercentOutput, -0.80);
+    intakeMotor.set(TalonSRXControlMode.PercentOutput, -0.75);
   }
   public static void intakeOut(){
-    intakeMotor.set(TalonSRXControlMode.PercentOutput,0.80);
+    intakeMotor.set(TalonSRXControlMode.PercentOutput,0.75);
   }
   public static void intakeStop(){
-    intakeMotor.set(TalonSRXControlMode.PercentOutput,.1);
+    intakeMotor.set(TalonSRXControlMode.PercentOutput,0);
   }
   
   public void runIntake(double speed){
